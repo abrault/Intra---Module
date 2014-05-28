6f893aa5eb78096741d49ff2e0484ecbadcea84b
abrault <abrault@student.42.fr>
2014-05-28 02:07:48 +0200

    Add base module

---
+++ app/views/module/create.blade.php
@@ -0,0 +1,5 @@
+@extends('template.main')
+
+@section('content')
+<a class="btn btn-primary" href="#" role="button">Create a new Module</a>
+@stop
---
+++ app/views/module/index.blade.php
@@ -0,0 +1,7 @@
+@extends('template.main')
+
+@section('content')
+<?php
+echo "Action index";
+?>
+@stop
