4b16fe361bd3b8b85eb98c4b78d494e0b8384999
Oshi <abrault@student.42.fr>
2014-06-04 11:28:15 +0200

    Update ModuleController.php

--- app/controllers/ModuleController.php
+++ app/controllers/ModuleController.php
@@ -63,41 +63,10 @@ class ModuleController extends BaseController {
 		//
 	}
 
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
+	public function delete($id)
 	{
-		//
+		$user = Module::find($id);
+		$user->delete();
+		return Redirect::action('ModuleController@index');
 	}
-
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		return View::make('hello.php');
-	}
-
-
 }
